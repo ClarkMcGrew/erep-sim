4b18b60f61f2c22ba03781684a119c9a65d67937
Clark McGrew <clark.mcgrew@stonybrook.edu>
2021-01-29 12:13:17 -0500

    Make the "erep-merge" main program into the main erep-sim

--- app/erepSim.cxx
+++ app/erepSim.cxx
@@ -10,6 +10,7 @@
 #include <TTree.h>
 #include <TChain.h>
 #include <TGeoManager.h>
+#include <TRandom.h>
 
 #include <iostream>
 #include <sstream>
@@ -17,24 +18,136 @@
 #include <memory>
 #include <unistd.h>
 
+/// The command is:
+///
+/// erep-merge -o output.root -n <count> -M <file>:<mean> -N <file>:<number>
+void usage() {
+    std::cout << "usage: erep-merge -o output.root [options] [input.root]"
+              << std::endl;
+    std::cout << std::endl;
+    std::cout << "   -o [output-file]   Set the output file" << std::endl;
+    std::cout << "   -r [run-number]    Set the run number" << std::endl;
+    std::cout << "   -n [event-count]   Events to generate" << std::endl;
+    std::cout << "   -M [file]:[mean]   Draw 'mean' events from file"
+              << std::endl;
+    std::cout << "   -N [file]:[number] Draw exactly 'number' events from file"
+              << std::endl;
+    std::cout << std::endl;
+    std::cout << "  If a separate input file is provided, it is equivalent to"
+              << "  adding '-N input.root:1'."
+              << std::endl;
+}
+
+// Which files to get edep-sim events frm.
+struct EventTree {
+    EventTree() : eventCount(0), firstEntry(0), lastEntry(0), treeEntry(0) {}
+    std::string fileName;   // The file name.
+    double      eventCount; // Events to draw from the file
+    int         firstEntry; // First entry in the chain.
+    int         lastEntry;  // firstEntry + total entries in chain
+    int         treeEntry;  // The last entry used.
+};
+
+// Fill a vector of random entries to be used in an event.  This has
+// protection for when there aren't enough entries in the input file.
+void randomEntryVector(std::vector<int>& entries, int choose,
+                       int totalEntries) {
+    entries.clear();
+    ///////////////////////////////////////////////////////
+    // Not enough entries, so return them all.
+    if (totalEntries < choose) {
+        for (int i=0; i<totalEntries; ++i) {
+            entries.push_back(i);
+        }
+        return;
+    }
+    ///////////////////////////////////////////////////////
+    // Only a few entries, so use an elimination method.
+    if (totalEntries < 5*choose) {
+        // Add all possible entries
+        for (int i=0; i<totalEntries; ++i) {
+            entries.push_back(i);
+        }
+        // Eliminate entries until we have few enough.
+        while (entries.size() > (std::size_t) choose) {
+            int eliminate = entries.size() - choose;
+            for (int i = 0; i<eliminate; ++i) {
+                int entry = gRandom->Uniform(0,entries.size());
+                // Mark an entry to be dropped with the vector size.  This
+                // will sort to be the last element in the vector.
+                entries[entry] = entries.size();
+            }
+            std::sort(entries.begin(), entries.end());
+            std::vector<int>::iterator e
+                = std::unique(entries.begin(),entries.end());
+            // Drop an extra entry since the "drop" flag will be the one
+            // before e.
+            entries.erase(e-1,entries.end());
+        }
+        return;
+    }
+    ///////////////////////////////////////////////////////
+    // Lots of entries, so add entries until we have enough.
+    int remaining = choose;
+    while (remaining > 0) {
+        for (int i=0; i<remaining; ++i) {
+            int entry = gRandom->Uniform(0,totalEntries);
+            entries.push_back(entry);
+        }
+        std::sort(entries.begin(), entries.end());
+        std::vector<int>::iterator e
+            = std::unique(entries.begin(),entries.end());
+        entries.erase(e,entries.end());
+        remaining = choose - entries.size();
+    }
+}
+
 int main(int argc, char **argv) {
     std::string outputName;
-    int maxEvents = 1E+8; // Maximum to process.
-    int combineEntries = 1; // Number of input entries per output event.
     int runId = 0;
+    // Maximum number of erep-sim events to generate.
+    int maxEvents = 1E+8;
+    // All of the inputs.
+    std::vector<EventTree> inputFiles;
+    // If true, then don't use the ecal/stt kludges.
+    int sanity = false;
+    // The detectors that will be simulated during this run.
+    std::vector<std::shared_ptr<ERepSim::DetectorBase>> gDetectors;
+
 
     while (true) {
-        int c = getopt(argc,argv,"c:n:o:r:");
+        int c = getopt(argc,argv,"M:n:N:o:r:S");
         if (c<0) break;
         switch (c) {
-        case 'c': {
-            std::istringstream tmp(optarg);
-            tmp >> combineEntries;
+        case 'M': {
+            std::string file(optarg);
+            std::string count(file.substr(file.find(':')+1));
+            file = file.substr(0,file.find(':'));
+            std::istringstream tmp(count);
+            EventTree et;
+            et.fileName = file;
+            double eCount = 1.0;
+            tmp >> eCount;
+            et.eventCount = eCount; // positive says random (this is the mean).
+            inputFiles.push_back(et);
             break;
         }
         case 'n': {
             std::istringstream tmp(optarg);
             tmp >> maxEvents;
+            break;
+        }
+        case 'N': {
+            std::string file(optarg);
+            std::string count(file.substr(file.find(':')+1));
+            file = file.substr(0,file.find(':'));
+            std::istringstream tmp(count);
+            EventTree et;
+            et.fileName = file;
+            int eCount = 1;
+            tmp >> eCount;
+            et.eventCount = -eCount; // negative says fixed (this is the value).
+            inputFiles.push_back(et);
             break;
         }
         case 'o': {
@@ -44,6 +157,11 @@ int main(int argc, char **argv) {
         case 'r': {
             std::istringstream tmp(optarg);
             tmp >> runId;
+            break;
+        }
+        case 'S': {
+            // Impose sanity and don't use the ecal kludges.
+            sanity = true;
             break;
         }
         default: {
@@ -56,112 +174,98 @@ int main(int argc, char **argv) {
         }
         }
     }
-    if (combineEntries < 1) combineEntries = 1;
-
-    if (argc <= optind) {
-        throw std::runtime_error("Missing input file");
-    }
-    std::vector<std::string> inputNames;
-    while (optind < argc) {
-        inputNames.push_back(argv[optind++]);
-        std::cout << "Input Name " << inputNames.back() << std::endl;
-    }
-
 
     if (outputName.empty()) {
         std::cout << "Set the output file name using the '-o' option"
                   << std::endl;
         throw std::runtime_error("Must have an output file");
     }
-    std::cout << "Output Name " << outputName << std::endl;
 
-    // Attach to the input tree.
-    std::unique_ptr<TFile> inputFile(
-        new TFile(inputNames.front().c_str(),"old"));
-    if (!inputFile->IsOpen()) throw std::runtime_error("Input file not open");
+    while (optind < argc) {
+        EventTree et;
+        et.fileName = argv[optind++];
+        et.eventCount = -1;
+        inputFiles.push_back(et);
+    }
 
-    /// Build the input chain.
+    // Attach to the first input file so we can get things like the geometry.
+    std::unique_ptr<TFile>
+        firstFile(new TFile(inputFiles.front().fileName.c_str(),"old"));
+    if (!firstFile->IsOpen()) throw std::runtime_error("Input file not open");
+
+    // Get the geometry.
+    if (!dynamic_cast<TGeoManager*>(firstFile->Get("EDepSimGeometry"))) {
+        std::runtime_error("No EDepSimGeometry");
+    }
+
+    // Look for the ecal kludge
+    std::unique_ptr<TChain> ecalKludgeChain;
+    if (!sanity && dynamic_cast<TTree*>(firstFile->Get("tDigit"))) {
+        ecalKludgeChain.reset(new TChain("tDigit"));
+    }
+
+    // Build the monster chain.
     std::unique_ptr<TChain> edepsimChain(new TChain("EDepSimEvents"));
-    for (std::size_t i = 0; i<inputNames.size(); ++i) {
-        std::cout << "Add EDepSim file " << inputNames[i] << std::endl;
-        edepsimChain->Add(inputNames[i].c_str());
+    int totalEntries = 0;
+    for (EventTree& et : inputFiles) {
+        std::cout << "Input Name: " << et.fileName << std::endl;
+        std::cout << "    -- ";
+        if (et.eventCount < 0) {
+            std::cout << " Count: " << -et.eventCount;
+        }
+        else {
+            std::cout << " Mean: " << et.eventCount;
+        }
+        et.firstEntry = totalEntries;
+        et.treeEntry = et.firstEntry;
+        edepsimChain->Add(et.fileName.c_str());
+        totalEntries = edepsimChain->GetEntries();
+        et.lastEntry = totalEntries;
+        std::cout << " Entries: [" << et.firstEntry
+                  << " - " << et.lastEntry << ")";
+        if (ecalKludgeChain) {
+            ecalKludgeChain->Add(et.fileName.c_str());
+            int ecalEntries = ecalKludgeChain->GetEntries();
+            std::cout << " w/ ECal ";
+            if (ecalEntries != totalEntries) {
+                throw std::runtime_error("File doesn't support ecal kludge");
+            }
+        }
+        std::cout << std::endl;
     }
 
     static TG4Event* edepsimEvent = NULL;
     edepsimChain->SetBranchAddress("Event",&edepsimEvent);
-    int totalEntries = edepsimChain->GetEntries();
 
-    TGeoManager* geom
-        = dynamic_cast<TGeoManager*>(inputFile->Get("EDepSimGeometry"));
-    if (!geom) {
-        std::runtime_error("No EDepSimGeometry");
-    }
-
-    // Check to see if the ECal tree in in the input file.
-    TChain* ecalDigitChain = NULL;
-    try {
-        TTree* ecalDigitTree = dynamic_cast<TTree*>(inputFile->Get("tDigit"));
-        if (!ecalDigitTree) {
-            std::runtime_error("File does not have ECal hits");
-        }
-        /// Build the input chain.
-        ecalDigitChain = new TChain("tDigit");
-        for (std::size_t i = 0; i<inputNames.size(); ++i) {
-            std::cout << "Add ECal file " << inputNames[i] << std::endl;
-            ecalDigitChain->Add(inputNames[i].c_str());
-        }
-        std::cout << "File has the ECal electronics simulation"
-                  << std::endl;
-    }
-    catch (...) {
-        std::cout << "File does not have the ECal electronics simulation"
-                  << std::endl;
-    }
-
-    std::vector<std::shared_ptr<ERepSim::DetectorBase>> detectors;
-
-#define PROCESS_ECAL
-#ifdef PROCESS_ECAL
-    if (ecalDigitChain) {
+    if (ecalKludgeChain) {
         std::cout << "ERepSim:: Create ECal." << std::endl;
-        detectors.push_back(
+        gDetectors.push_back(
             std::shared_ptr<ERepSim::DetectorECal>(
-                new ERepSim::DetectorECal(ecalDigitChain)));
+                new ERepSim::DetectorECal(ecalKludgeChain.get())));
     }
-    else {
-        std::cout << "ERepSim:: Invalid ECal digit chain." << std::endl;
-    }
-#endif
-
-#define PROCESS_3DST
-#ifdef PROCESS_3DST
-    detectors.push_back(
+    gDetectors.push_back(
         std::shared_ptr<ERepSim::Detector3DST>(
             new ERepSim::Detector3DST));
-#endif
-
-#define PROCESS_TPC
-#ifdef PROCESS_TPC
-    detectors.push_back(
+    gDetectors.push_back(
         std::shared_ptr<ERepSim::DetectorTPC>(
             new ERepSim::DetectorTPC("TPC",
                                      "voltpc",
                                      "volTpc_PV_0",
                                      ERepSim::Def::Detector::kTPC)));
-    detectors.push_back(
+    gDetectors.push_back(
         std::shared_ptr<ERepSim::DetectorTPC>(
             new ERepSim::DetectorTPC("TopTPC",
                                      "voltpcTop",
                                      "volTpcTop_PV_0",
                                      ERepSim::Def::Detector::kTopTPC)));
-    detectors.push_back(
+    gDetectors.push_back(
         std::shared_ptr<ERepSim::DetectorTPC>(
             new ERepSim::DetectorTPC("BotTPC",
                                      "voltpcBot",
                                      "volTpcBot_PV_0",
                                      ERepSim::Def::Detector::kBotTPC)));
-#endif
 
+    std::cout << "Output Name " << outputName << std::endl;
     std::unique_ptr<TFile> outputFile(new TFile(outputName.c_str(),"recreate"));
     ERepSim::Output::Get().CreateTrees();
 
@@ -169,39 +273,88 @@ int main(int argc, char **argv) {
     gGeoManager->Write();
 
     // Initialize all of the detectors.
-    for (auto detector: detectors) {detector->Initialize();}
+    for (std::shared_ptr<ERepSim::DetectorBase>& detector : gDetectors) {
+        detector->Initialize();
+    }
 
-    // Build the output events.
-    int entry = 0;
-    for (int erepEvent = 0;
-         erepEvent < maxEvents && entry < totalEntries;
-         ++erepEvent) {
-        std::cout << "ERepSim::  Event " << erepEvent
-                  << " starting from entry " << entry
+    // Make the events...
+    std::vector<int> selectedEntries;
+    std::vector<int> possibleEntries;
+    for (int eventId = 0; eventId < maxEvents; ++eventId) {
+        std::cout << "ERepSim::  Event " << runId << "/" << eventId
                   << std::endl;
         // Reset the detectors for the next event.
-        ERepSim::Output::Get().Reset(runId,erepEvent);
-        for (auto detector: detectors) {detector->Reset();}
+        ERepSim::Output::Get().Reset(runId,eventId);
+        for (std::shared_ptr<ERepSim::DetectorBase>& detector : gDetectors) {
+            detector->Reset();
+        }
 
-        // Add input events to build the full ERepSim event.
-        for (int subEntry = 0; subEntry < combineEntries; ++subEntry) {
-            if (edepsimChain->GetEntry(entry++) < 1) {
-                std::cout << "Ran out of input events"
+        // Select the entries to use from each input source.
+        selectedEntries.clear();
+        for (EventTree& et : inputFiles) {
+            // Check if we are using a fixed number of entries.
+            if (et.eventCount < 0) {
+                std::cout << "ERepSim:: file: " << et.fileName;
+                std::cout << std::endl;
+                std::cout << "     --" << " Use: " << -et.eventCount
+                          << " starting at " << et.treeEntry
                           << std::endl;
-                break;
+                for (int i = 0; i < -et.eventCount; ++i) {
+                    selectedEntries.push_back(et.treeEntry++);
+                }
+                continue;
             }
-            std::cout << "ERepSim:: Handle input event "
+            int events = gRandom->Poisson(et.eventCount);
+            std::cout << "ERepSim:: file: " << et.fileName;
+            std::cout << std::endl;
+            std::cout << "     --" << " Mean: " << et.eventCount
+                      << " Generate: " << events
+                      << std::endl;
+            possibleEntries.clear();
+            randomEntryVector(possibleEntries,events,
+                              et.lastEntry-et.firstEntry);
+            for (int entry : possibleEntries) {
+                selectedEntries.push_back(entry+et.firstEntry);
+            }
+        }
+
+        // Apply a sanity check to see if the event should be generated.
+        bool eventOK = true;
+        for (EventTree& et : inputFiles) {
+            if (et.treeEntry > et.lastEntry) {
+                std::cout << "ERepSim:: Ran out of events in "
+                          << et.fileName
+                          << std::endl;
+                eventOK = false;
+            }
+        }
+        if (!eventOK) break;
+
+        // The selected entries should already be sorted, but check anyway.
+        std::sort(selectedEntries.begin(), selectedEntries.end());
+
+        // Accumulate the input events.
+        for (int entry : selectedEntries) {
+            if (edepsimChain->GetEntry(entry) < 1) {
+                std::cout << "Error getting entry " << entry
+                          << std::endl;
+                throw std::runtime_error("Error getting an input event");
+            }
+            std::cout << "ERepSim:: Entry: " << entry
+                      << " Input event: "
                       << edepsimEvent->RunId << "/" << edepsimEvent->EventId
                       << std::endl;
             ERepSim::Output::Get().Update(edepsimEvent);
-            for (auto detector: detectors) {
+            for (std::shared_ptr<ERepSim::DetectorBase>& detector
+                     : gDetectors) {
                 detector->Accumulate(entry, edepsimEvent);
             }
         }
 
         // Apply the sensors and DAQ
-        for (auto detector: detectors) {
-            detector->Process(erepEvent);
+        for (std::shared_ptr<ERepSim::DetectorBase>& detector
+                 : gDetectors) {
+            detector->Process(eventId);
         }
 
         ERepSim::Output::Get().Fill();
